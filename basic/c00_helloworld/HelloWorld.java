e65d58ec89db943551e3c2494439d9da3e7447ba
David <160413129+Dvkllz@users.noreply.github.com>
2026-08-26 00:29:06 -0500

    style: quitar comentarios de HelloWorld.java

--- basic/c00_helloworld/HelloWorld.java
+++ basic/c00_helloworld/HelloWorld.java
@@ -1,15 +1,5 @@
 package basic.c00_helloworld;
 
-/*
-Clase 12 - Hola Mundo
-Vídeo: https://youtu.be/JOAqpdM36wI?t=4250
-
-Clase 13 - Comentarios
-Vídeo: https://youtu.be/JOAqpdM36wI?t=4937
-*/
-
-// Hola Mundo
-
 public class HelloWorld {
 
     public static void main(String[] args) {

27e29de00df4dcd4e669de91abc0c820084b5a6d
David <160413129+Dvkllz@users.noreply.github.com>
2026-08-26 00:29:21 -0500

    style: quitar comentarios de DataTypes.java

--- basic/c01_beginner/DataTypes.java
+++ basic/c01_beginner/DataTypes.java
@@ -1,23 +1,14 @@
 package basic.c01_beginner;
 
-/*
-Clase 16 - Tipos de datos primitivos
-Vídeo: https://youtu.be/JOAqpdM36wI?t=6211
-*/
-
 public class DataTypes {
 
     public static void main(String[] args) {
-
-        // Tipos de datos primitivos
 
         int myInt = 37;
         System.out.println(myInt);
 
         double myDouble = 1.77;
         System.out.println(myDouble);
-
-        // float, long, byte
 
         char myChar = 'a';
         System.out.println(myChar);
@@ -28,8 +19,6 @@ public class DataTypes {
 
         String myString = "Hola, Java";
         System.out.println(myString);
-
-        // Tipo de dato en tiempo de compilación
 
         System.out.println(myString.getClass().getSimpleName());
     }

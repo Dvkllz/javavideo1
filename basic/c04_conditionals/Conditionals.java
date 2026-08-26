5543b93f2b433e0916adc63e09f92421aa158c9f
David <160413129+Dvkllz@users.noreply.github.com>
2026-08-26 00:35:33 -0500

    Ajustes finales

--- basic/c04_conditionals/Conditionals.java
+++ basic/c04_conditionals/Conditionals.java
@@ -1,22 +1,8 @@
 package basic.c04_conditionals;
 
-/*
-Clase 35 - Condicionales
-Vídeo: https://youtu.be/JOAqpdM36wI?t=9908
-*/
-
 public class Conditionals {
 
     public static void main(String[] args) {
-
-        // Condicionales
-
-        /*
-        Clase 36 - Sentencia if
-        Vídeo: https://youtu.be/JOAqpdM36wI?t=9978
-        */
-
-        // if, else if, else
 
         var age = 18;
 
@@ -29,13 +15,6 @@ public class Conditionals {
         } else {
             System.out.println("El usuario es menor de edad");
         }
-
-        /*
-        Clase 37 - Sentencia switch
-        Vídeo: https://youtu.be/JOAqpdM36wI?t=10678
-        */
-
-        // switch
 
         var day = 5;
 
